b14e7a4b473fe8d2a83b922d8a3e56c67c022f81
Lihis <lihis@lihis.net>
2014-08-07 17:34:01 +0300

    Syncing with deshonor

---
+++ u8800pro.mk
@@ -0,0 +1,12 @@
+# Inherit CM common Phone stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit device configuration.
+$(call inherit-product, device/huawei/u8800pro/device_u8800pro.mk)
+
+# Device identifier.
+PRODUCT_DEVICE := u8800pro
+PRODUCT_NAME := u8800pro
+PRODUCT_BRAND := huawei
+PRODUCT_MODEL := u8800pro
+PRODUCT_MANUFACTURER := huawei
