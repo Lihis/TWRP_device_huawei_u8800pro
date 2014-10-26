9ea2bc5760cd6866a077f00965393b92e02be087
Lihis <lihis@lihis.net>
2014-10-26 15:44:08 +0200

    CHANGE: Product names

--- u8800pro.mk
+++ u8800pro.mk
@@ -7,6 +7,5 @@ $(call inherit-product, device/huawei/u8800pro/device_u8800pro.mk)
 # Device identifier.
 PRODUCT_DEVICE := u8800pro
 PRODUCT_NAME := u8800pro
-PRODUCT_BRAND := Huawei
-PRODUCT_MODEL := u8800pro
+PRODUCT_MODEL := U8800Pro
 PRODUCT_MANUFACTURER := Huawei
