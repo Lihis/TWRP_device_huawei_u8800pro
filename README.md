81099656ccaf214969325716fa2001b9e761b963
Tomi Lähteenmäki <thomazuu@gmail.com>
2016-02-26 09:20:53 +0200

    Bump to 3.0.0

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-TWRP 2.8.1.0 device tree (4.4.4)
+TWRP 3.0.0 device tree
 ================================
 android_device_huawei_u8800pro
 --------------------------------
-Thanks for Blefish supporting our old device's.
 
-Thanks to desalesouche, you have inspired me a lot. Thanks to Dees_Troy and bigbiff about helping me fix things. And of course to the TeamWinRecoveryProject.
+Thanks to desalesouche, you have inspired and helped me a lot. Thanks to Dees_Troy and bigbiff about helping me fix things. And of course to the TeamWinRecoveryProject.
--- cm.mk
+++ cm.mk
@@ -1,11 +1,14 @@
-# Inherit from our omni product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit CM common GSM stuff.
+#$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit CM common Phone stuff
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration.
 $(call inherit-product, device/huawei/u8800pro/device_u8800pro.mk)
 
 # Device identifier.
 PRODUCT_DEVICE := u8800pro
-PRODUCT_NAME := omni_u8800pro
+PRODUCT_NAME := cm_u8800pro
 PRODUCT_MODEL := U8800Pro
 PRODUCT_MANUFACTURER := Huawei
